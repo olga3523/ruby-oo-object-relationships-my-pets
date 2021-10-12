0dc949e69a5b0a64397151e29980ce2796d0d58b
OLGA <olichka23.85@gmail.com>
2021-10-12 11:32:11 -0400

    commit tree 6ef60b3bc29579e636d112554ef63fdfcacff94d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
-  # code goes here
+
+  attr_reader :name
+  attr_accessor :mood, :owner
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+    owner.dogs << self
+  end
+
+  def self.all()
+    return @@all
+  end 
+  
 end
